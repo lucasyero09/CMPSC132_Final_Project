465cd708d1b5776db247988ade93ba9a97ff312b
Lucas Yero <lucasyero09@gmail.com>
2026-04-28 21:45:27 -0400

    Implemented filtering and additional display

--- Final_Project.py
+++ Final_Project.py
@@ -36,16 +36,71 @@ class LaptopScraper:
         
         self.laptops = laptops
 
-        def display_filtered(self):
-            if not self.filteredlaptops:
-                raise RuntimeError("No filtered laptops found. Run filter() first.")
 
-            #Iterates through the dictionary and prints
-            for laptop in self.filteredlaptops:
-                print(f"Laptop: {laptop['name']}")
-                print(f"Price: {laptop['price']}")
-                print(f"Rating: {laptop['rating']} / 5")
-                print("-" * 30)
+    def display(self):
+        if not self.laptops:
+            raise RuntimeError("No laptops found. Run scrape() first.")
+
+        #Iterates through the dictionary and prints
+        for laptop in self.laptops:
+            print(f"Laptop: {laptop['name']}")
+            print(f"Price: {laptop['price']}")
+            print(f"Rating: {laptop['rating']} / 5")
+            print("-" * 30)
+
+    def display_filtered(self):
+        if not self.filteredlaptops:
+            raise RuntimeError("No filtered laptops found. Run filter() first.")
+
+        #Iterates through the dictionary and prints
+        for laptop in self.filteredlaptops:
+            print(f"Laptop: {laptop['name']}")
+            print(f"Price: {laptop['price']}")
+            print(f"Rating: {laptop['rating']} / 5")
+            print("-" * 30)
+
+
+    @property
+    def _name_filter(self):
+        #Returns sorted list extracting the name from the nested dict and using that as the sorting criteria
+        return sorted(self.laptops, key=lambda x: x['name'])
+
+    @property
+    def _price_filter(self):
+        #Returns sorted list extracting the price and cleaning it by removing the $ from the nested dict and using that as the sorting criteria
+        return sorted(self.laptops, key=lambda x: float(x['price'].replace('$', '')))
+
+    @property
+        #Returns sorted list extracting the rating from the nested dict and using that as the sorting criteria
+    def _rating_filter(self):
+        return sorted(self.laptops, key=lambda x: int(x['rating']))
+    
+    def filter(self, method, amount):
+
+        VALID_FILTERS = ['Laptop', 'Price', 'Rating']
+
+        #Input checks
+        if method not in VALID_FILTERS:
+            raise ValueError(f"Invalid filter method '{method}'. Choose from: 'Laptop', 'Price', 'Rating'")
+        if amount is not None and not type(amount) == int:
+            raise ValueError(f"Invalid amount '{amount}'. Please choose only non-zero positive integers or None")
+        if amount is not None and amount <= 0:
+            raise ValueError(f"Invalid amount '{amount}'. Please choose only non-zero positive integers or None")
+                
+        #Checks for each type of filter method
+        if method == 'Laptop':
+            self.filteredlaptops = self._name_filter
+        elif method == 'Price':
+            self.filteredlaptops = self._price_filter
+        elif method == 'Rating':
+            self.filteredlaptops = self._rating_filter
+
+        #Filtering for the amount, keeping the amount listed
+        if type(amount) == int:
+            self.filteredlaptops = self.filteredlaptops[:amount]
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
+
 
 
 
